6e1c24b114c52fb298f9468b78139491d36da536
nikfp <nik@nikfp.com>
2023-12-13 01:17:27 +0000

    working on day 5 part 2

--- day5/base.exs
+++ day5/base.exs
@@ -1,7 +1,7 @@
 test_file =
   File.read!("./test.txt")
 
-prod_file = 
+prod_file =
   File.read!("./prod.txt")
 
 defmodule AOC do
@@ -27,6 +27,7 @@ defmodule AOC do
           [dest, start, offset] = Enum.map(x, fn y -> String.to_integer(y) end)
           {dest, start, offset}
         end)
+        |> Enum.sort(fn {_, x, _}, {_, y, _} -> x <= y end)
       end)
 
     {seed_values, map_values}
@@ -55,10 +56,11 @@ defmodule AOC do
     offsetters = mapper_list |> Enum.map(&AOC.build_offsetter/1)
 
     fn x ->
-      {_, value } = Enum.reduce(offsetters, {:pending, x}, fn offsetter, acc ->
-        # offsetter
-        offsetter.(acc)
-      end)
+      {_, value} =
+        Enum.reduce(offsetters, {:pending, x}, fn offsetter, acc ->
+          # offsetter
+          offsetter.(acc)
+        end)
 
       value
     end
@@ -67,31 +69,62 @@ defmodule AOC do
   def build_pipeline(transformer_inputs) do
     transformers = transformer_inputs |> Enum.map(&AOC.build_transformer/1)
 
-      fn x -> 
-        Enum.reduce(transformers, x, fn transformer, acc -> 
-          transformer.(acc)
-        end)
-      end
+    fn x ->
+      Enum.reduce(transformers, x, fn transformer, acc ->
+        transformer.(acc)
+      end)
     end
+  end
+
+  def build_comparison_list(mapper_settings) do
+    mapper_settings |> Enum.map(fn {_, begin, offset} -> {begin, begin + offset - 1} end)
+  end
 end
 
-solver_1 = fn x ->
+# solver_1 = fn x ->
+#   {inputs, mapper_settings} =
+#     x
+#     |> AOC.parse_input()
+#
+#
+#   pipeline = AOC.build_pipeline(mapper_settings)
+#   #
+#   #
+#   inputs
+#   |> Enum.map(fn x -> pipeline.(x) end)
+#   |> Enum.min()
+# end
+solver_2 = fn x ->
   {inputs, mapper_settings} =
     x
     |> AOC.parse_input()
 
-  pipeline = AOC.build_pipeline(mapper_settings)
-  #
-  #
-  inputs |> Enum.map(fn x -> pipeline.(x) end)
-  |> Enum.min()
-  end
+  compare_lists =
+    mapper_settings
+    |> Enum.map(&AOC.build_comparison_list/1)
 
+  first_set =
+    inputs
+    |> Enum.chunk_every(2)
+    |> Enum.map(fn [a, b] -> {a, a + b - 1} end)
+    |> hd()
+
+  first_compare = hd(compare_lists)
+  {first_set, first_compare}
+end
+
+# compare lower bound of input range to first transform lower bound
+# if input range LB is lower, compare input LB to transform UB
+# if it's still lower, skip to the next transformer. 
+
+
+# test_file
+# |> solver_1.()
+# |> IO.inspect()
 test_file
-|> solver_1.()
+|> solver_2.()
 |> IO.inspect()
 
-prod_file
-|> solver_1.()
-|> IO.inspect()
-
+# prod_file
+# |> solver_1.()
+# |> IO.inspect()
